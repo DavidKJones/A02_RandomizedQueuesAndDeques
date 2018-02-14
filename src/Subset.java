26c5f6dc5e63385848cbccdeb8b047269cb3350a
DavidJones <emaildkj@gmail.com>
2018-02-14 16:59:31 -0700

    Finished Subset.java

--- src/Subset.java
+++ src/Subset.java
@@ -10,23 +10,38 @@ public class Subset
 	{
 		RandomizedQueue<String> rQ = new RandomizedQueue<String>();
 		
-		StdOut.println("Please enter the number of strings you want to create:");
+		//get integer k from the command line
+		int k = Integer.parseInt(args[0]);
+		String list = "";
+		
+		//get the value of N
+		StdOut.println("Type in the number of strings you want to queue.");
 		int N = StdIn.readInt();
-		String[] str = new String[N];
+		//we want to insure that N is greater than or equal to k
+		while(N < k)
+		{
+			StdOut.println("Number of strings needs to be greater or equal to k.");
+			N = StdIn.readInt();
+		}
+		
+		//enqueue the items
 		for(int i = 0; i < N; i++)
 		{
-			str[i] = StdIn.readString();
-			rQ.enqueue(str[i]);
+			String str = StdIn.readString();
+			rQ.enqueue(str);
+			list += " " + str;
 		}
 		
-		
-		//print randomized queue
-		StdOut.print("% echo ");
-		for(String s : rQ)
+		//print the list and print k items randomly
+		StdOut.println("% echo" + list + " | java Subset " + k);			
+		int count = 0;
+		for( String s : rQ )
 		{
-			StdOut.print(String.format("%s ", s));
+			System.out.println(s);
+			count++;
+			if(count == k)
+				break;
 		}
-		StdOut.print(String.format("| java Subset %d\n", N));
 	}
 
 }
